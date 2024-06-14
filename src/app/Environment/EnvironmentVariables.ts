b5f752fd7b195d4befbd00ed64b4ddf41e58f22c
Simon <73712759+ZvSimon@users.noreply.github.com>
2024-06-14 22:30:16 +0200

    let's deploy

---
+++ src/app/Environment/EnvironmentVariables.ts
@@ -0,0 +1,17 @@
+export const EnvironmentalVariables = {
+  production: false,
+
+  //Base Urls to access the end points using the http client get method
+  weatherApiLocationBaseURL:
+    'https://weather338.p.rapidapi.com/locations/search?',
+  weatherApiForecastBaseURL:
+    'https://weather338.p.rapidapi.com/weather/forecast?',
+
+  //Variables for API key name and value
+  xRapidApiKeyName: 'X-RapidAPI-Key',
+  xRapidApikeyValue: '598d735cedmshcb9df8e02af2ebap1b49fajsn3227f223eeef',
+
+  //Variables for host name and value
+  xRapidApiHostName: 'X-RapidAPI-Host',
+  xRapidApiHostValue: 'weather338.p.rapidapi.com',
+};
